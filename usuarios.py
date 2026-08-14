9d65ea19dc51c5c3fc4740b957d7ab7163270625
villanuevacarlosedardo422-prog <villanuevacarloseduardo422@gamail.com>
2026-08-14 09:30:01 -0500

    ya se recibe el nombre de los usuarios

---
+++ usuarios.py
@@ -0,0 +1,5 @@
+cuantos = int(input("Ingrese la cantidad de usuarios que desea registrar: "))
+
+for i in range(cuantos):
+    nombre = input("Ingrese el nombre del usuario: ")
+    usuarios.append(nombre)
